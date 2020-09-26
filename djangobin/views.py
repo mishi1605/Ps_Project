923f35d52474fb67fe44567d4caf805e95d996c5
mansi <mansi.jain.1605@gmail.com>
2020-09-27 01:19:35 +0530

    table incorporated

--- djangobin/views.py
+++ djangobin/views.py
@@ -3,8 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-import subprocess
 
+
+
+import subprocess
+# from django.http import JsonResponse
+
+import json
 #This method is acting like basic index.html for every project
 def index(request):
      return render(request,'index.html')
@@ -54,44 +59,85 @@ def db(request):
     return HttpResponse(random.randint(0,100));
 
 def maxcpu(request):
-    #TO DO: Return the json object in response
-    #filter the command to get the required things
-    process = subprocess.Popen('ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6', 
+    process = subprocess.Popen('ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6 | tail -n 5', 
     shell=True, 
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE )
-    arr_cpu =[]
+    arr_cpu = " "
     for a in process.stdout:
         json_str = {'pid' :'', 'ppid': ''}
-        str = a.split();
-        # print str
-        json_str['pid'] = str[0]
-        json_str['ppid'] = str[1]
-        json_str['name'] = str[2]
-        json_str['percent_util'] = str[3]
-        print json_str
-        arr_cpu.append(json_str)
+        print type(json_str)
+        stri = a.split();
+        print stri
+        json_str['pid'] = stri[0]
+        json_str['ppid'] = stri[1]
+        json_str['name'] = stri[2]
+        json_str['cpu_utilization'] = stri[3]
+        # print json_str
+        # print "\n"
+        # print type(json_str)
+        json_str = json.dumps(json_str)
+        # print "print json dumps type"
+        # print type(json_str)
+        # print json_str
+        # print type(str(json_str))
+        new_str = str(json_str)
+        # print new_str
+        arr_cpu = arr_cpu + "?" + new_str
+        # print arr_cpu
+    
+    # print "\n"
+    # print "\n"
+    # print arr_cpu 
+    # print type(arr_cpu)
+    # print "print its type"
+    # print type(str(arr_cpu))
+    # arr_cpu - list of string (1 string - json obj)
+    # 
+    # designacao= json.dumps( arr_cpu, cls=DjangoJSONEncoder)
+    # return Response (json.loads(designacao))
+    #I did this because I wanted to get rid of the backslash that was appearing in the json!!
+    # arr = "-".join(arr_cpu)
+    # print "\n"
+    # print "\n"
+    str_new = str(arr_cpu)
+    # print str_new
+    return HttpResponse(str_new)
 
-    return HttpResponse(arr_cpu)
 
 def maxmem(request):
     #TO DO: Return the json object in response
     #filter the command to get the required things
-    process = subprocess.Popen('ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6', 
+    process = subprocess.Popen('ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6 | tail -n 5', 
     shell=True, 
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE )
-    arr_mem =[]
+
+    arr_mem = " "
+    
     for a in process.stdout:
-        json_str = {'pid': '', 'ppid': ''}
-        str = a.split();
+        json_str = {'pid' :'', 'ppid': ''}
+        # print type(json_str)
+        stri = a.split();
         # print str
-        json_str['pid'] = str[0]
-        json_str['ppid'] = str[1]
-        json_str['name'] = str[2]
-        json_str['percent_util'] = str[3]
-        print json_str
-        arr_mem.append(json_str)
-    return HttpResponse(arr_mem)
+        json_str['pid'] = stri[0]
+        json_str['ppid'] = stri[1]
+        json_str['name'] = stri[2]
+        json_str['memory_utilization'] = stri[3]
+        # print json_str
+        # print "\n"
+        # print type(json_str)
+        json_str = json.dumps(json_str)
+        # print "print json dumps type"
+        # print type(json_str)
+        # print json_str
+        # print type(str(json_str))
+        new_str = str(json_str)
+        arr_mem = arr_mem + "#" + new_str
+        str_new = str(arr_mem)
+    # print str_new
+    # return HttpResponse(process.stdout)
+   
+    return HttpResponse(str_new)
 
 
--- static/js/myChart.js
+++ static/js/myChart.js
@@ -215,14 +215,35 @@ function doRunmaxcpu() {
 })
     .then(function (response) {
        
-        document.getElementById("topcpu").innerHTML = response.data;
+        // document.getElementById("a").innerHTML = response.data;
+       // console.log("in javascript code")
+        console.log(response.data)
+        // console.log(typeof respone.data);
+        var str_a = response.data
+
+        var arr = str_a.split("#")
+        var arr_fin = []
+        
+        for(var i = 1; i < arr.length; i++){
+            // console.log("in for loop \n");
+           // console.log(arr[i]);
+           // console.log(arr[i+1]);
+           // console.log(typeof arr[i])
+            arr_fin.push(JSON.parse(arr[i]));
+
+        }
+        // console.log("printing arr fin")
+        // console.log(arr_fin)
+        // array of json objects
+        CreateTableFromJSON(arr_fin)
         console.log(response);
     })
 
     .catch(function (error) {
         console.log(' error=', error.message);
     });
-}
+};
+
 function doRunmaxmem() {
     console.log("executed ls -la");
     axios({
@@ -234,18 +255,172 @@ function doRunmaxmem() {
 })
     .then(function (response) {
        
-        document.getElementById("topmem").innerHTML = response.data;
+        // document.getElementById("topmem").innerHTML = response.data;
+        // console.log(response);
+        var str_a = response.data
+
+        var arr = str_a.split("#")
+        var arr_fin = []
+        // // var arr = response.data
+        for(var i = 1; i < arr.length; i++){
+           //  console.log("in for loop \n");
+           // console.log(arr[i]);
+           // console.log(arr[i+1]);
+           // console.log(typeof arr[i])
+            arr_fin.push(JSON.parse(arr[i]));
+
+        }
+        // console.log("printing arr fin")
+        // console.log(arr_fin)
+        // array of json objects
+        CreateTableFromJSON1(arr_fin)
         console.log(response);
     })
 
     .catch(function (error) {
         console.log(' error=', error.message);
     });
-}
+};
+
 // doSomething();
 setInterval(doRunmaxcpu, 5000);
 setInterval(doRunmaxmem, 5000);
 
 
 
+function CreateTableFromJSON(myBooks) {
+        console.log(typeof myBooks);
+        // var myBooks = [
+        //     {
+        //         "Book ID": "1",
+        //         "Book Name": "Computer Architecture",
+        //         "Category": "Computers",
+        //         "Price": "125.60"
+        //     },
+        //     {
+        //         "Book ID": "2",
+        //         "Book Name": "Asp.Net 4 Blue Book",
+        //         "Category": "Programming",
+        //         "Price": "56.00"
+        //     },
+        //     {
+        //         "Book ID": "3",
+        //         "Book Name": "Popular Science",
+        //         "Category": "Science",
+        //         "Price": "210.40"
+        //     }
+        // ]
 
+        // EXTRACT VALUE FOR HTML HEADER. 
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var col = [];
+
+        console.log(myBooks.length)
+        for (var i = 0; i < myBooks.length; i++) {
+            console.log(typeof myBooks[i])
+            for (var key in myBooks[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < myBooks.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = myBooks[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("topcpu");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    }
+
+
+
+
+function CreateTableFromJSON1(myBooks) {
+        console.log(typeof myBooks);
+        // var myBooks = [
+        //     {
+        //         "Book ID": "1",
+        //         "Book Name": "Computer Architecture",
+        //         "Category": "Computers",
+        //         "Price": "125.60"
+        //     },
+        //     {
+        //         "Book ID": "2",
+        //         "Book Name": "Asp.Net 4 Blue Book",
+        //         "Category": "Programming",
+        //         "Price": "56.00"
+        //     },
+        //     {
+        //         "Book ID": "3",
+        //         "Book Name": "Popular Science",
+        //         "Category": "Science",
+        //         "Price": "210.40"
+        //     }
+        // ]
+
+        // EXTRACT VALUE FOR HTML HEADER. 
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var col = [];
+
+        console.log(myBooks.length)
+        for (var i = 0; i < myBooks.length; i++) {
+            console.log(typeof myBooks[i])
+            for (var key in myBooks[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < myBooks.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = myBooks[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer1 = document.getElementById("topmem");
+        divContainer1.innerHTML = "";
+        divContainer1.appendChild(table);
+    }
